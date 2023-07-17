411eafee86a2519db52719a9b813d3973523500a
Shiitake <shiitake.github@proton.me>
2023-07-17 11:12:13 +0200

    Added "getFileSize" function

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def welcomeScreen(): #A simple welcomescreen
         print('Hello my friend!')
         time.sleep(0.1)
         welcome -= 1
+    selectFile()
 
 def selectFile(): #Let the user select a file to be feed
     clear()
@@ -21,11 +22,19 @@ def selectFile(): #Let the user select a file to be feed
     time.sleep(2)
     Tk().withdraw()
     filePath = askopenfilename()
+    getFileSize(filePath)
 
 def getFileSize(file = None): #Gets the file size with the os.path.getsize function.
-    #fileSize = os.path.getsize(file)
-    #return fileSize
-    pass
+    if file == None:
+        print("Please select a file.")
+    else:
+        pass
+
+    try:
+        fileSize = os.path.getsize(file)
+        return fileSize
+    except:
+        print("An error occurred (Code: 1)")
 
 def FEED_ME(file): #Will make the file bigger with junk
     pass
